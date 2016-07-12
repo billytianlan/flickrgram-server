765815708417cc561d23e781ef5992a1c7e33ec1
Billy Lan <billylan22@gmail.com>
2016-07-12 11:05:05 -0700

    limit photo results and order by date dsc

--- server/photos/photoController.js
+++ server/photos/photoController.js
@@ -2,11 +2,13 @@ let Photo = require('../db/db').Photo
 
 let getAllPhotos = (req, res) => {
   console.log('inside the photo controller');
-  Photo.findAll()
-    .then((photos) => {
-      photos = photos.slice(0, 10)
-      res.send(photos);
-    })
+  Photo.findAll({ 
+    limit: 20,
+    order: 'createdAt DESC'
+  })
+  .then((photos) => {
+    res.send(photos);
+  })
 }
 
 module.exports = {
